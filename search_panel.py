d0fd3572ff676b241aa920db72871653860caa41
Linux Panda <23082189+PandaCuddles@users.noreply.github.com>
2020-08-09 16:30:55 -0500

    Program now cleans up temp images on exit

--- search_panel.py
+++ search_panel.py
@@ -70,7 +70,12 @@ class AnimuSearchPanel(wx.Panel):
         closed.
 
         """
-        os.rm
+        image_folder = f"{os.getcwd()}/images/"
+
+        images = [image for image in os.listdir(image_folder) if image.endswith(".jpg")]
+
+        for image in images:
+            os.remove(os.path.join(image_folder, image))
         os._exit(1)
         # sys.exit()
         pass
